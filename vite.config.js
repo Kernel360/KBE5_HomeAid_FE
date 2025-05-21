427ab9c18cd33355988dd92388989c4532a4e045
soohoioa <soohoioa@gmail.com>
2025-05-21 16:44:16 +0900

    feat: Vite + React + 플러그인 포함 초기 프로젝트 세팅

---
+++ vite.config.js
@@ -0,0 +1,16 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import svgr from "vite-plugin-svgr";
+
+export default defineConfig({
+  plugins: [react(), svgr()],
+  server: { port: 3000, open: true },
+  resolve: {
+    alias: {
+      "@": "/src",
+      "@components": "/src/components",
+      "@pages": "/src/pages",
+      "@assets": "/src/assets",
+    },
+  },
+});
