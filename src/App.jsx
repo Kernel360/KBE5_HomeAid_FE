5739da6681c568245c866d0624ef80779207eb51
soohoioa <soohoioa@gmail.com>
2025-06-03 17:16:36 +0900

    refactor: 디렉토리 구조 변경

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,10 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import CustomerBoardList from './features/customer/board/BoardList';
-import CustomerBoardWrite from './features/customer/board/BoardWrite';
-import CustomerBoardDetail from './features/customer/board/BoardDetail';
-import ManagerBoardList from './features/manager/board/BoardList';
-import ManagerBoardWrite from './features/manager/board/BoardWrite';
-import ManagerBoardDetail from './features/manager/board/BoardDetail';
+import { BrowserRouter } from 'react-router-dom';
+import { AppRoutes } from './routes';
 
 function App() {
   return (
     <BrowserRouter>
-      <Routes>
-        <Route
-          path="/"
-          element={
-            <div className="text-3xl text-blue-500">Tailwind 테스트</div>
-          }
-        />
-
-        <Route path="/customer/board" element={<CustomerBoardList />} />
-        <Route path="/customer/board/write" element={<CustomerBoardWrite />} />
-        <Route path="/customer/board/:id" element={<CustomerBoardDetail />} />
-        <Route
-          path="/customer/board/:id/edit"
-          element={<CustomerBoardWrite />}
-        />
-        <Route path="/manager/board" element={<ManagerBoardList />} />
-        <Route path="/manager/board/write" element={<ManagerBoardWrite />} />
-        <Route path="/manager/board/:id" element={<ManagerBoardDetail />} />
-        <Route path="/manager/board/:id/edit" element={<ManagerBoardWrite />} />
-      </Routes>
+      <AppRoutes />
     </BrowserRouter>
   );
 }
