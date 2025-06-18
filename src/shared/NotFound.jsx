9b49f8b0588b4828419be59a704395de4e7e53ca
anjihyeon <jhan-@naver.com>
2025-06-18 11:05:06 +0900

    chore: 404추가 처리방안 구상해야함

--- src/shared/NotFound.jsx
+++ src/shared/NotFound.jsx
@@ -1,10 +1,13 @@
 import React from 'react';
-
+import { useNavigate } from 'react-router-dom';
 export const NotFound = () => {
+  const navaigate = useNavigate();
+
   return (
     <div>
       <h1>404 - Page Not Found</h1>
       <p>The page you are looking for does not exist.</p>
+      <button onClick={()=> navaigate('/')}>홈으로</button>
     </div>
   );
 };
